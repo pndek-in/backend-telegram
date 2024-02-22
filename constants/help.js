865387397a3f8ef7f665c64a9445f962fb68b80f
jal <jaluwibowoaji@gmail.com>
2024-02-22 21:00:18 +0700

    fix: typo in help

--- constants/help.js
+++ constants/help.js
@@ -4,7 +4,7 @@ Welcome to the pndek.in Bot!
 Available commands:
   - /start : Start the bot
   - /help : Show this help message
-  - /set_token : Set your unique token, you can get your token from https://pndek.in/dashboard/settings
+  - /set_token : Set your unique token, you can get your token from https://app.pndek.in/dashboard/settings
   ⚠️ Remember that setting your token again will overwrite your previous token in this account !!!
   - /create : Create a new short link
   - /list : List all your active short links
